19e001bf9d9285c6bee56d1df7d53dbe93236d7a
zmgu <122958434+zmgu@users.noreply.github.com>
2025-10-27 10:36:26 +0900

    플랫폼 서비스 프로젝트 생성

---
+++ unduck-platform-service/src/main/java/com/ex/unduckplatformservice/UnduckPlatformServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ex.unduckplatformservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnduckPlatformServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnduckPlatformServiceApplication.class, args);
+    }
+
+}
---
+++ unduck-platform-service/src/test/java/com/ex/unduckplatformservice/UnduckPlatformServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.unduckplatformservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnduckPlatformServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
