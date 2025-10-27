b99d55ee415237d26da0a5b96e665cb444122c62
zmgu <122958434+zmgu@users.noreply.github.com>
2025-10-27 10:57:17 +0900

    Auth 서비스 프로젝트 생성

---
+++ unduck-auth-service/src/main/java/com/ex/unduckauthservice/UnduckAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ex.unduckauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnduckAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnduckAuthServiceApplication.class, args);
+    }
+
+}
---
+++ unduck-auth-service/src/test/java/com/ex/unduckauthservice/UnduckAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.unduckauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnduckAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
