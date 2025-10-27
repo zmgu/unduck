48e8f932cb2ab9d44ffbbbc0663f2260051c1162
zmgu <122958434+zmgu@users.noreply.github.com>
2025-10-27 10:28:29 +0900

    게이트웨이 서비스 프로젝트 생성

---
+++ unduck-gateway-service/src/main/java/com/ex/unduckgatewayservice/UnduckGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ex.unduckgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnduckGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnduckGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ unduck-gateway-service/src/test/java/com/ex/unduckgatewayservice/UnduckGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.unduckgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnduckGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
