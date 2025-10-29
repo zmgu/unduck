5e41dde94f68bb949ebb2718ac930d060cc67cc9
zmgu <122958434+zmgu@users.noreply.github.com>
2025-10-29 15:06:16 +0900

    unduck-config-service 생성

---
+++ backend/unduck-config-service/src/test/java/com/ex/unduckconfigservice/UnduckConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.unduckconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnduckConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
