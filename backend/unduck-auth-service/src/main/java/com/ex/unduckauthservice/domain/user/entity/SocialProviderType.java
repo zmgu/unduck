d6e7285f2a3c02e30e4618927a84a9d149ced735
zmgu <122958434+zmgu@users.noreply.github.com>
2025-11-07 17:55:40 +0900

    enum 클래스 생성

---
+++ backend/unduck-auth-service/src/main/java/com/ex/unduckauthservice/domain/user/entity/SocialProviderType.java
@@ -0,0 +1,5 @@
+package com.ex.unduckauthservice.domain.user.entity;
+
+public enum SocialProviderType {
+    NAVER, KAKAO, GOOGLE;
+}
---
+++ backend/unduck-auth-service/src/main/java/com/ex/unduckauthservice/domain/user/entity/UserRoleType.java
@@ -0,0 +1,5 @@
+package com.ex.unduckauthservice.domain.user.entity;
+
+public enum UserRoleType {
+    USER, ADMIN
+}
